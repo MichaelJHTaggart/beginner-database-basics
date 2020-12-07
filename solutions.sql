a1a70f753244fee0c66be1fd186c757df2889144
Michael Taggart <mtaggarthawk@gmail.com>
2020-12-07 14:44:59 -0700

    initial commit

---
+++ solutions.sql
@@ -0,0 +1,73 @@
+-- Instructions
+-- Create a table called person that records a person's id, name, age, height ( in cm ), city, favorite_color.
+-- id should be an auto-incrementing id/primary key - Use type: SERIAL
+-- Add 5 different people into the person database.
+-- Remember to not include the person_id because it should auto-increment.
+-- List all the people in the person table by height from tallest to shortest.
+-- List all the people in the person table by height from shortest to tallest.
+-- List all the people in the person table by age from oldest to youngest.
+-- List all the people in the person table older than age 20.
+-- List all the people in the person table that are exactly 18.
+-- List all the people in the person table that are less than 20 and older than 30.
+-- List all the people in the person table that are not 27 (Use not equals).
+-- List all the people in the person table where their favorite color is not red.
+-- List all the people in the person table where their favorite color is not red and is not blue.
+-- List all the people in the person table where their favorite color is orange or green.
+-- List all the people in the person table where their favorite color is orange, green or blue (use IN).
+-- List all the people in the person table where their favorite color is yellow or purple (use IN).
+-- Solution
+
+
+-- Table - orders
+-- Instructions
+-- Create a table called orders that records: order_id, person_id, product_name, product_price, quantity.
+-- Add 5 orders to the orders table.
+-- Make orders for at least two different people.
+-- person_id should be different for different people.
+-- Select all the records from the orders table.
+-- Calculate the total number of products ordered.
+-- Calculate the total order price.
+-- Calculate the total order price by a single person_id.
+-- Solution
+
+
+
+-- Table - artist
+-- Instructions
+-- Add 3 new artists to the artist table. ( It's already created )
+-- Select 10 artists in reverse alphabetical order.
+-- Select 5 artists in alphabetical order.
+-- Select all artists that start with the word 'Black'.
+-- Select all artists that contain the word 'Black'.
+-- Solution
+
+
+
+
+
+-- Table - employee
+-- Instructions
+-- List all employee first and last names only that live in Calgary.
+-- Find the birthdate for the youngest employee.
+-- Find the birthdate for the oldest employee.
+-- Find everyone that reports to Nancy Edwards (Use the ReportsTo column).
+-- You will need to query the employee table to find the Id for Nancy Edwards
+-- Count how many people live in Lethbridge.
+-- Solution
+--  SQL Solutions
+-- Table - invoice
+-- Instructions
+-- Count how many orders were made from the USA.
+-- Find the largest order total amount.
+-- Find the smallest order total amount.
+-- Find all orders bigger than $5.
+-- Count how many orders were smaller than $5.
+-- Count how many orders were in CA, TX, or AZ (use IN).
+-- Get the average total of the orders.
+-- Get the total sum of the orders.
+-- Solution
+--  SQL Solutions
+-- Resources
+--  SQL
+-- Contributions
+-- If you see a problem or a typo, please fork, make the necessary changes, and create a pull request so we can review your changes and merge them into the master repo and branch.
